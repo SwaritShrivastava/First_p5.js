38ce083a544815662f78828057f4584ec6767e7a
Swarit Shrivastava <73647160+SwaritShrivastava@users.noreply.github.com>
2021-05-08 08:36:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function take_snapshot(){
+save('Image.png');
+}
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.size(300, 300);
+  video.hide();
+  modelLoaded();
+}
+function modelLoaded(){
+    console.log("ModelLoaded!");
+}
+function draw(){
+    image(video,0,0,300,200);
+}
+webcam=document.getElementById("camera");
